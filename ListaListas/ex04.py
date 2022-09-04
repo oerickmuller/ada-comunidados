1e3c8efbc5c6ed604c8eb90b071802c84133e720
Erick Muller <erick@em.pro.br>
2022-09-04 14:54:47 -0300

    Terminando Exercicio 04 - Listas

---
+++ ListaListas/ex04.py
@@ -0,0 +1,6 @@
+"""
+Calcule o valor da raiz quadrada dos 100 primeiros numeros inteiros diferentes de zero.
+"""
+
+for numero in range(1, 101):
+    print(f"Raiz quadrada de {numero} é {numero ** (1/2)}")
